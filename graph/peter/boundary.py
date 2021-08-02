5fa86584ca4742fb758fedd03c4dbca9b073c461
cathaypacific8747 <58929011+cathaypacific8747@users.noreply.github.com>
2021-08-03 01:24:22 +0800

    update boundary code

--- graph/peter/boundary.py
+++ graph/peter/boundary.py
@@ -2,6 +2,7 @@ import json
 from fastkml import kml
 from rich import inspect, print
 from shapely.geometry import Polygon
+from shapely.ops import unary_union
 from commons import common
 
 k = kml.KML()
@@ -9,19 +10,19 @@ ns = '{http://www.opengis.net/kml/2.2}'
 d = kml.Document(ns)
 k.append(d)
 
-with open('boundary/DCCA2019.json', 'r') as f:
-    for consituency in json.load(f):
-        if 'H' in consituency['CACODE']:
+with open('boundary/boundary.json', 'r') as f:
+    polygons = []
+    for consituency in json.load(f)["features"]:
+        if consituency["properties"]["地區號碼"] == 'H':
             bounds = []
-            for y, x in consituency['json_geometry']['coordinates'][0]:
-                coords = common.Coordinate(x=x, y=y)
-                bounds.append((coords.lon, coords.lat, 0)) # google reverses this
+            for lon, lat in consituency["geometry"]["coordinates"][0]:
+                bounds.append((lon, lat, 0))
 
-            p = kml.Placemark(ns, id=consituency['CACODE'], name=consituency['ENAME'])
+            p = kml.Placemark()
             p.geometry = Polygon(bounds)
             d.append(p)
-            # inspect(bounds)
+
+            break
 
 with open('boundary/boundary.kml', 'w+', encoding='utf-8') as fil:
-    print(k.to_string(prettyprint=True))
     fil.write(k.to_string(prettyprint=True))
---
+++ graph/peter/boundary/dep/boundary.py
@@ -0,0 +1,42 @@
+import json
+from fastkml import kml
+from rich import inspect, print
+from shapely.geometry import Polygon
+from shapely.ops import unary_union
+from commons import common
+
+k = kml.KML()
+ns = '{http://www.opengis.net/kml/2.2}'
+d = kml.Document(ns)
+k.append(d)
+
+# with open('boundary/DCCA2019.json', 'r') as f:
+#     for consituency in json.load(f):
+#         if 'H' in consituency['CACODE']:
+#             bounds = []
+#             for y, x in consituency['json_geometry']['coordinates'][0]:
+#                 coords = common.Coordinate(x=x, y=y)
+#                 bounds.append((coords.lon, coords.lat, 0))
+
+#             p = kml.Placemark(ns, id=consituency['CACODE'], name=consituency['ENAME'])
+#             p.geometry = Polygon(bounds)
+#             d.append(p)
+            # inspect(bounds)
+
+with open('boundary/DCCA2019.json', 'r') as f:
+    polygons = []
+    for consituency in json.load(f):
+        if 'H' in consituency['CACODE']:
+            polygons.append(Polygon(consituency['json_geometry']['coordinates'][0]))
+
+    for y, x in unary_union(polygons).exterior.coords:
+    bounds = []
+        coords = common.Coordinate(x=x, y=y)
+        bounds.append((coords.lon, coords.lat, 0))
+
+    p = kml.Placemark()
+    p.geometry = Polygon(bounds)
+    d.append(p)
+
+with open('boundary/boundary.kml', 'w+', encoding='utf-8') as fil:
+    fil.write(k.to_string(prettyprint=True))
