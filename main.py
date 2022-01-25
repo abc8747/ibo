cdfead6015a1727aa7691d891261f39d953b1dd0
cathaypacific8747 <abrahamcheung2004@gmail.com>
2022-01-26 07:20:45 +0800

    add: basic word extraction

--- main.py
+++ main.py
@@ -1,19 +1,35 @@
 import os
 import fitz
-import rich
+from rich import print
 
 class Document:
     def __init__(self, dir:str, docid:str) -> None:
         self.dir = dir
         self.docid = docid
-        print(f'Processing {self.docid}')
+        # print(f'Processing {self.docid}')
     
-    def populateData(self):
-        for page in fitz.open(self.dir):
-            print(page)
+    def extractWords(self):
+        self.words = []
+        for p in fitz.open(self.dir):
+            t = p.get_text('dict')
+            if 'blocks' not in t: continue
+            for b in t['blocks']:
+                if 'lines' not in b: continue
+                for l in b['lines']:
+                    if 'spans' not in l: continue
+                    for s in l['spans']:
+                        self.words.append({
+                            'size': s['size'],
+                            'text': s['text'],
+                        })
+    
+    def organiseQuestions(self):
+        for w in self.words:
+            print(w)
 
-inDir = 'data/sr'
-for srFile in os.listdir(inDir):
-    doc = Document(dir=os.path.join(inDir, srFile), docid=os.path.splitext(srFile)[0])
-    doc.populateData()
-    break
+if __name__ == '__main__':
+    inDir = 'data/sr'
+    for srFile in os.listdir(inDir):
+        doc = Document(dir=os.path.join(inDir, srFile), docid=os.path.splitext(srFile)[0])
+        doc.extractWords()
+        doc.organiseQuestions()
